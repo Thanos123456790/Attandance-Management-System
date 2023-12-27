10deb3e253ce7915eba5897817c1594795aea751
Subhadip Hazra <129437065+Thanos123456790@users.noreply.github.com>
2023-12-28 03:23:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-function generateTable() {
-    const startRollNo = parseInt(document.getElementById('startRollNo').value);
-    const totalStudents = parseInt(document.getElementById('totalStudents').value);
+let startRollNo;
+let totalStudents;
 
-    let tableHTML = '<table class="table table-striped"><thead><tr><th>Roll No</th><th>Attendance</th></tr></thead><tbody>';
+function generateTable() {
+    startRollNo = parseInt(document.getElementById('startRollNo').value);
+    totalStudents = parseInt(document.getElementById('totalStudents').value);
+
+    let tableHTML = '<table class="table table-striped"><thead><tr><th>Roll No</th><th>Attendance</th><th>Action</th></tr></thead><tbody>';
 
     for (let i = 0; i < totalStudents; i++) {
         const rollNo = startRollNo + i;
-        tableHTML += `<tr><td>${rollNo}</td><td><input type="checkbox" id="attendance_${rollNo}" name="attendance_${rollNo}"></td></tr>`;
+        tableHTML += `<tr><td>${rollNo}</td><td><input type="checkbox" id="attendance_${rollNo}" name="attendance_${rollNo}"></td><td><button onclick="deleteRow(this)">Delete</button></td></tr>`;
     }
 
+    tableHTML += '<tr><td colspan="3"><button onclick="addRow()">Add Row</button></td></tr>';
     tableHTML += '</tbody></table>';
+
     document.getElementById('attendanceTable').innerHTML = tableHTML;
+    document.getElementById('attendanceForm').style.display = 'none';
+}
+
+function deleteRow(btn) {
+    const row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+function addRow() {
+    const table = document.querySelector('.table tbody');
+    const lastRollNo = startRollNo + totalStudents - 1;
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `<td>${lastRollNo + 1}</td><td><input type="checkbox" id="attendance_${lastRollNo + 1}" name="attendance_${lastRollNo + 1}"></td><td><button onclick="deleteRow(this)">Delete</button></td>`;
+
+    table.insertBefore(newRow, document.querySelector('.table tbody tr:last-child'));
+    totalStudents++;
+
+    // Reorder the "Add Row" button to be at the end
+    table.appendChild(document.querySelector('.table tbody tr:last-child'));
 }
